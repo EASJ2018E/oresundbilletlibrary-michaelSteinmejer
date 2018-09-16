9b6c8409dd0c1c4f4b905ab3e7f45517dc3896e8
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-16 18:28:09 +0200

    done

---
+++ Oeresundbro/OeresundMC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BilletLibrary;
+
+namespace Oeresundbro
+{
+    public class OeresundMC: Transportmiddel
+    {
+        public OeresundMC(string nummerplade, DateTime dato, bool brobizz) : base(nummerplade, dato, brobizz)
+        {
+            if (nummerplade.Length >= 8)
+            {
+                throw new SystemException("Din nummerplade er forkert");
+            }
+            Nummerplade = nummerplade;
+            Dato = dato;
+            Brobizz = brobizz;
+        }
+
+        public override double Pris()
+        {
+            return Brobizz ? 73 : 210;
+        }
+
+        public override string Køretøj()
+        {
+            return "Øresund MC";
+        }
+    }
+}
---
+++ Oeresundbro/Oeresundbil.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using BilletLibrary;
+
+namespace Oeresundbro
+{
+    public class Oeresundbil: Transportmiddel
+    {
+        public Oeresundbil(string nummerplade, DateTime dato, bool brobizz) : base(nummerplade, dato, brobizz)
+        {
+            if (nummerplade.Length >= 8)
+            {
+                throw new SystemException("Din nummerplade er forkert");
+            }
+            Nummerplade = nummerplade;
+            Dato = dato;
+            Brobizz = brobizz;
+        }
+
+        public override double Pris()
+        {
+            return Brobizz ? 161 : 410;
+        }
+
+        public override string Køretøj()
+        {
+            return "Øresund Bil";
+        }
+    }
+}

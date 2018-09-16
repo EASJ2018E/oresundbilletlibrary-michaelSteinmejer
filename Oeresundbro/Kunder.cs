af886d25b68ed64497ce59edcd31528bb1b5784a
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-16 20:01:31 +0200

    done done done

---
+++ Oeresundbro/Kunder.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BilletLibrary;
+
+namespace Oeresundbro
+{
+    public class Kunder
+    {
+        public string Nummerplade { get; set; }
+
+        public List<Transportmiddel> Køretøjliste;
+
+        public Kunder()
+        {
+            Køretøjliste = new List<Transportmiddel>();
+        }
+
+       
+        public void Add(Transportmiddel trans)
+        {
+            Køretøjliste.Add(trans);
+        }
+
+       public double PrisSum()
+       {
+           double pris = 0;
+            foreach (var transp in Køretøjliste)
+            {
+                if (transp.Nummerplade == Nummerplade)
+                {
+                     pris = pris + transp.Pris();
+                    
+
+                }
+               
+            }
+            return pris;
+       }
+    }
+
+    
+}
